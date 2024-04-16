6895e6b625a0295417f176bc54d7c226a9d957bc
Matthew <matthew@dyn908-70.guest.ic.ac.uk>
2024-04-16 10:28:11 +0100

    Initial skeleton commit

---
+++ app/pages/1_Setup.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+with st.form(key="setup-form"):
+    uploaded_files = st.file_uploader("Upload polling data spreadsheet", accept_multiple_files=True)
+
+    policy_brief = st.text_input("Provide the policy brief")
+
+    other_issues = st.text_input("What other issues are in the news?")
+
+    submitted = st.form_submit_button("Do generative AI")
+
+    if submitted:
+        st.session_state.policy_brief = policy_brief
+        st.session_state.other_issues = other_issues
+        st.session_state.polling_data = uploaded_files
---
+++ app/utils/news.py
@@ -0,0 +1,7 @@
+from newscatcherapi import NewsCatcherApiClient
+
+def get_trending_news():
+    pass
+
+def get_relevant_news():
+    pass
